5d90d36abbd1a5eb5e4cc187109e5ac8ec1b9b38
aiuchi <hokuto100retsuken@gmail.com>
2019-08-24 10:21:22 +0900

    配列で日にちをaddしてものを比較

--- kagomokuNo23/ClientApp/src/app/datetimes/datetimes.component.ts
+++ kagomokuNo23/ClientApp/src/app/datetimes/datetimes.component.ts
@@ -12,13 +12,18 @@ export class DateTimesComponent {
   public targetDate = new Date(2019, 8, 24);
   public comparisonDate: Date = new Date(2019, 8, 24);
 
-  moment: moment.Moment;
+  momentDateList: moment.Moment[] = [];
 
   constructor() {
-    
+    for (let i = 0; i < 5; i++) {
+      const date = moment('2019-08-20');
+      date.add(i, 'days');
+      this.momentDateList.push(date);
+    }
+    console.log(this.momentDateList);
   }
 
-  doComparisionByDate(target: Date, comparision: Date):boolean {
+  doComparisionByDate(target: Date, comparision: Date): boolean {
     return target === comparision;
   }
 
@@ -35,5 +40,14 @@ export class DateTimesComponent {
 
   doComparisionByMomentIsSame(target: Date, comparision: Date): boolean {
     return moment(target).isSame(moment(comparision));
+  }
+
+  doComparisionByMomentIsSameForMoment(target: Date, comparision: moment.Moment): boolean {
+    return moment(target).isSame(comparision);
+  }
+
+  doComparisionByMomentIsSameForString(target: Date, comparision: string): boolean {
+    console.log(comparision);
+    return moment(target).isSame(comparision);
   }
 }
